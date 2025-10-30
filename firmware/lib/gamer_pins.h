f68d08bb7fbc768492a603266433450291ace8da
Luke Sbityakov <93800822+apotatoa@users.noreply.github.com>
2025-10-30 12:57:52 -0400

    Added header file that defines our current pins

---
+++ firmware/lib/gamer_pins.h
@@ -0,0 +1,41 @@
+// Pin definitions for handheld game device
+
+
+#define SELECT      (0)
+#define BOOT        (0)
+#define ABAT        (1)
+#define START       (2)
+
+// ADC pins
+#define ACC1        (4)
+#define ACC2        (5)
+#define AY          (6)
+#define AX          (7)
+
+// i2c pins
+#define SDA         (8)
+#define SCL         (9)
+
+// display pins
+#define DIS8        (10)
+#define DIS7        (11)
+#define DIS6        (12)
+#define DIS5        (13)
+#define DIS4        (14)
+#define DIS3        (23)
+#define DIS2        (47)
+#define DIS1        (48)
+
+
+#define ChgStat     (16)
+
+// GPIO inputs (buttons)
+#define UP          (17)
+#define RIGHT       (18)
+#define DOWN        /// TODO
+#define LEFT        /// TODO
+#define A           (39)
+#define B           (40)
+#define X           (41)
+#define Y           (42)
+
